02a2f73cbab16fcc3a5ec02e8d84b555aa98c7d1
PradeepRaj <pradeeprajr93@gmail.com>
2021-06-02 06:16:16 +0000

    Removed unnecessary scripts

--- src/assets/scripts/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-env=`python3 -c 'import json, os;
-env_variables={}
-for key, value in iter(os.environ.items()):
-  if(key.startswith("API_CONNECTION")):
-    env_variables[key]=value
-print(json.dumps(env_variables))'`
-echo $env > /app/dist/my-notes/assets/env.json
